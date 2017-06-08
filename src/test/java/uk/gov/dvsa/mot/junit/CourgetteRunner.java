fe1f23a02f193b65618040d9e725ed184cd7058b
Jamie Kelly <Jamie.Kelly@github.com>
2017-06-08 15:11:18 +0100

    Revert "Changed to use Cucumber Extended Parallel Runner"
    This reverts commit 53d49359901f0ff2943338c1fb6455fe631b9d53.

---
+++ src/test/java/uk/gov/dvsa/mot/junit/CourgetteRunner.java
@@ -0,0 +1,31 @@
+package uk.gov.dvsa.mot.junit;
+
+import courgette.api.CourgetteOptions;
+import courgette.api.CourgetteRunLevel;
+import courgette.api.junit.Courgette;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+/**
+ * Junit/Courgette-JVM runner used to run the Gerkin features.
+ *
+ * <code>runLevel</code> of <code>FEATURE</code> results in one independent copy of the Spring Application Context
+ * being created for each cucumber **feature** (so shared between scenarios within each feature).
+ */
+@RunWith(Courgette.class)
+@CourgetteOptions(
+        threads = 1,
+        runLevel = CourgetteRunLevel.FEATURE,
+        rerunFailedScenarios = false,
+        showTestOutput = true,
+        cucumberOptions = @CucumberOptions(
+                features = "features",
+                glue = "uk.gov.dvsa.mot",
+                plugin = {
+                        "pretty",
+                        "json:build/reports/selenium/selenium.json",
+                        "html:build/reports/selenium"
+                }
+        ))
+public class CourgetteRunner {
+}
