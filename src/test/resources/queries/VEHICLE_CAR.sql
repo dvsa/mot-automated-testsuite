bf3d9a97e40439e0270675598a50ab795b0049b4
chris.nappin <chris@nappin.com>
2017-06-20 16:35:37 +0100

    Use latest MOT, and exclude cars in active test.

--- src/test/resources/queries/VEHICLE_CAR.sql
+++ src/test/resources/queries/VEHICLE_CAR.sql
@@ -1,10 +1,14 @@
 select veh.registration, veh.vin, mtc.odometer_value
-from vehicle veh, model_detail md, model, make, mot_test_current mtc
+from vehicle veh, model_detail md, model, make, (
+  select max(id) as id, vehicle_id  from mot_test_current
+  group by vehicle_id) as latest_mot, mot_test_current mtc
 where veh.model_detail_id = md.id
 and md.model_id = model.id
 and model.name is not null
 and model.make_id = make.id
 and model.name is not null
 and md.vehicle_class_id = 4 -- cars only
-and mtc.vehicle_id = veh.id -- with current MOT
-and mtc.odometer_result_type = 'OK' -- with successful previous odometer reading
+and veh.id = latest_mot.vehicle_id
+and mtc.id = latest_mot.id
+and mtc.odometer_result_type = 'OK'
+and mtc.status_id != 4 -- not cars already in active mot test
