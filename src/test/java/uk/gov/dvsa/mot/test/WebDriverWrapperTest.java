e7bfeaeec6f612f4b9c0d39cd3627b25a1b5e91a
chris.nappin <chris@nappin.com>
2017-07-14 17:25:35 +0100

    Added further WebDriverWrapper tests.

--- src/test/java/uk/gov/dvsa/mot/test/WebDriverWrapperTest.java
+++ src/test/java/uk/gov/dvsa/mot/test/WebDriverWrapperTest.java
@@ -15,6 +15,8 @@ import org.springframework.mock.env.MockEnvironment;
 import uk.gov.dvsa.mot.framework.WebDriverWrapper;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit tests for any thorny logic in the <code>WebDriverWrapper</code> class.
@@ -48,6 +50,149 @@ public class WebDriverWrapperTest {
     public void tearDown() {
         driverWrapper.preDestroy();
         driverWrapper = null; // trigger GC
+    }
+
+    /**
+     * Tests <code>getData()</code> and <code>setData()</code> in combination.
+     */
+    @Test
+    public void setAndGetData() {
+        driverWrapper.setData("key1", "aaa");
+        driverWrapper.setData("key2", "bbb");
+        driverWrapper.setData("key3", "ccc");
+
+        assertEquals("Wrong value returned", "aaa", driverWrapper.getData("key1"));
+        assertEquals("Wrong value returned", "bbb", driverWrapper.getData("key2"));
+        assertEquals("Wrong value returned", "ccc", driverWrapper.getData("key3"));
+    }
+
+    /**
+     * Tests that calling <code>setData()</code> with the same key replaces the previous value.
+     */
+    @Test
+    public void setDataReplaces() {
+        driverWrapper.setData("key1", "aaa");
+        driverWrapper.setData("key1", "bbb");
+
+        assertEquals("Wrong value returned", "bbb", driverWrapper.getData("key1"));
+    }
+
+    /**
+     * Tests that calling <code>getData()</code> with a key that hasn't been populated throws an exception.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void getDataNotSet() {
+        driverWrapper.getData("key1");
+    }
+
+    /**
+     * Tests <code>getAllDataKeys()</code> when data has been populated.
+     */
+    @Test
+    public void getAllDataKeysPopulated() {
+        driverWrapper.setData("key1", "aaa");
+        driverWrapper.setData("key2", "bbb");
+
+        List<String> expected = new ArrayList<>();
+        expected.add("key1");
+        expected.add("key2");
+
+        assertEquals("Wrong value returned", expected, driverWrapper.getAllDataKeys());
+    }
+
+    /**
+     * Tests <code>getAllDataKeys()</code> when data has not been populated.
+     */
+    @Test
+    public void getAllDataKeysEmpty() {
+        List<String> expected = new ArrayList<>();
+
+        assertEquals("Wrong value returned", expected, driverWrapper.getAllDataKeys());
+    }
+
+    /**
+     * Tests that <code>reset()</code> clears all data.
+     */
+    @Test
+    public void resetClearsData() {
+        driverWrapper.setData("key1", "aaa");
+        driverWrapper.setData("key2", "bbb");
+
+        driverWrapper.reset();
+
+        assertEquals("All data should have been cleared", 0, driverWrapper.getAllDataKeys().size());
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when the input is disabled.
+     */
+    @Test
+    public void isButtonDisabledInputYes() {
+        browseTo("/isButtonDisabled-1.html", "isButtonDisabled - 1");
+        assertTrue("Button is disabled", driverWrapper.isButtonDisabled("Button 2"));
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when the input is not disabled.
+     */
+    @Test
+    public void isButtonDisabledInputNo() {
+        browseTo("/isButtonDisabled-1.html", "isButtonDisabled - 1");
+        assertFalse("Button is not disabled", driverWrapper.isButtonDisabled("Button 3"));
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when the input is missing.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void isButtonDisabledInputMissing() {
+        browseTo("/isButtonDisabled-1.html", "isButtonDisabled - 1");
+        driverWrapper.isButtonDisabled("Button 4");
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when there are several inputs matching the button text.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void isButtonDisabledInputSeveral() {
+        browseTo("/isButtonDisabled-2.html", "isButtonDisabled - 2");
+        driverWrapper.isButtonDisabled("Button 1");
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when the button is disabled.
+     */
+    @Test
+    public void isButtonDisabledButtonYes() {
+        browseTo("/isButtonDisabled-3.html", "isButtonDisabled - 3");
+        assertTrue("Button is disabled", driverWrapper.isButtonDisabled("Button 2"));
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when the button is not disabled.
+     */
+    @Test
+    public void isButtonDisabledButtonNo() {
+        browseTo("/isButtonDisabled-3.html", "isButtonDisabled - 3");
+        assertFalse("Button is not disabled", driverWrapper.isButtonDisabled("Button 3"));
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when the button is missing.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void isButtonDisabledButtonMissing() {
+        browseTo("/isButtonDisabled-3.html", "isButtonDisabled - 3");
+        driverWrapper.isButtonDisabled("Button 4");
+    }
+
+    /**
+     * Tests <code>isButtonDisabled()</code>, when there are several buttons matching the button text.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void isButtonDisabledButtonSeveral() {
+        browseTo("/isButtonDisabled-4.html", "isButtonDisabled - 4");
+        driverWrapper.isButtonDisabled("Button 1");
     }
 
     /**
