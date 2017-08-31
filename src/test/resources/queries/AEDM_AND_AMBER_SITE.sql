e1dcec8cf488c599e6b8105dd3fc64bde26d68d5
chris.nappin <chris@nappin.com>
2017-08-31 14:12:44 +0100

    Fixed queries to use score from latest assessment correctly, avoids random test failures.

--- src/test/resources/queries/AEDM_AND_AMBER_SITE.sql
+++ src/test/resources/queries/AEDM_AND_AMBER_SITE.sql
@@ -1,9 +1,12 @@
 SELECT p.username, o.name, s.name
 FROM site s, organisation o, person p, organisation_business_role_map obrm,
-(SELECT max(id), site_id, site_assessment_score FROM enforcement_site_assessment GROUP BY site_id) as esa
-WHERE esa.site_assessment_score > 275.00
-AND esa.site_assessment_score <= 360
-AND s.id = esa.site_id
+  (SELECT max(id) as id, site_id FROM enforcement_site_assessment GROUP BY site_id) as latest_assessment,
+  enforcement_site_assessment esa
+WHERE latest_assessment.id = esa.id
+AND latest_assessment.site_id = s.id
+AND esa.id = latest_assessment.id
+AND esa.site_assessment_score > 270.00
+AND esa.site_assessment_score <= 360.00
 AND o.id = s.organisation_id
 AND obrm.organisation_id = o.id
 AND obrm.business_role_id = 1
