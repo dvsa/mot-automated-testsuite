7db297b598b256750a1c22183130c4804b4bfe39
chris.nappin <chris@nappin.com>
2017-09-18 15:19:12 +0100

    Fix user search and user display issue if user has no middle name.

--- src/test/resources/queries/TESTER_WITH_LICENCE.sql
+++ src/test/resources/queries/TESTER_WITH_LICENCE.sql
@@ -11,4 +11,6 @@ and p.username is not null -- exclude dodgy test data
 and p.driving_licence_id = l.id
 and l.licence_type_id = 1 -- driving licence
 and l.licence_country_id = 11 -- issued by GB
+and p.middle_name is not null -- avoid name formatting issues on user search result screen
+and p.middle_name not like '% %'
 limit 10
