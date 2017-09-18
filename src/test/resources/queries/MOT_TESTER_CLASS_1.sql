9cb024bf846325e9d20bdc6097e70e799defe0ed
chris.nappin <chris@nappin.com>
2017-09-18 11:14:14 +0100

    Updated drift clause to exclude beyond +/-2.

--- src/test/resources/queries/MOT_TESTER_CLASS_1.sql
+++ src/test/resources/queries/MOT_TESTER_CLASS_1.sql
@@ -26,7 +26,7 @@ and sc.security_card_status_lookup_id = 1 -- only assigned cards
 and not exists ( -- not all security_card have a corresponding security_card_drift
   select 1 from security_card_drift scd
   where sc.id = scd.security_card_id
-  and (scd.last_observed_drift >= 60 or scd.last_observed_drift <= -60) -- no drift forward or back > 1 hour
+  and (scd.last_observed_drift > 60 or scd.last_observed_drift < -60) -- no drift beyond +/-2
 )
 and not exists (
   select 1 from mot_test_current mtc
