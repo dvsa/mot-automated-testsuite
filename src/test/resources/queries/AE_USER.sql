b21208016c2fa5a7439ed54cd1016178b138c9dd
ibrahimjassat <ibrahim.jassat@dvsa.gov.uk>
2017-09-22 16:19:44 +0100

    sql query for finding an AE user with slots

---
+++ src/test/resources/queries/AE_USER.sql
@@ -0,0 +1,9 @@
+select DISTINCT afe.ae_ref as ae_ref, o.name as ae_name, o.`slots_balance`
+from organisation o, auth_for_ae afe, person aedm_user, organisation_business_role_map obrm
+where o.name is not null
+and afe.organisation_id = o.id
+and obrm.organisation_id = o.id
+and aedm_user.username is not null
+and obrm.person_id = aedm_user.id
+order by slots_balance asc
+limit 10
