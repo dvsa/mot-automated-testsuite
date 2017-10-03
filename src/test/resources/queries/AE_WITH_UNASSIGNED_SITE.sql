c6737fe324e73e4376f5fb88727d61a83fdec0e5
chris.nappin <chris@nappin.com>
2017-10-03 15:13:18 +0100

    Select unique AEs.

--- src/test/resources/queries/AE_WITH_UNASSIGNED_SITE.sql
+++ src/test/resources/queries/AE_WITH_UNASSIGNED_SITE.sql
@@ -1,14 +1,17 @@
-select afe.ae_ref as ae_ref, o.name as ae_name, unassigned_site.site_number as site_number,
-  unassigned_site.name as site_name
-from organisation o, auth_for_ae afe, site unassigned_site
-where o.name is not null
-and afe.organisation_id = o.id
-and unassigned_site.site_status_id = 1 -- approved site
-and not exists (
-  select * from organisation_site_map osm
-  where osm.organisation_id = o.id
-  and osm.site_id = unassigned_site.id
-)
-and unassigned_site.name not like 'VOSA %'
-and unassigned_site.name not like 'DVSA %'
+select ae_ref, ae_name, site_number, site_name from (
+    select afe.ae_ref as ae_ref, o.name as ae_name, unassigned_site.site_number as site_number,
+      unassigned_site.name as site_name
+    from organisation o, auth_for_ae afe, site unassigned_site
+    where o.name is not null
+    and afe.organisation_id = o.id
+    and unassigned_site.site_status_id = 1 -- approved site
+    and not exists (
+      select * from organisation_site_map osm
+      where osm.organisation_id = o.id
+      and osm.site_id = unassigned_site.id
+    )
+    and unassigned_site.name not like 'VOSA %'
+    and unassigned_site.name not like 'DVSA %'
+    limit 100000) o
+group by o.ae_ref
 limit 10
