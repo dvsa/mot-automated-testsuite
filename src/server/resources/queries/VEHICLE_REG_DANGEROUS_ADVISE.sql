4f53c42424fbefe439e48a207e60901324f0e8ff
markstan71 <mark.stansfield@bjss.com>
2017-10-23 10:39:39 +0100

    MOTH automation tests

---
+++ src/server/resources/queries/VEHICLE_REG_DANGEROUS_ADVISE.sql
@@ -0,0 +1,12 @@
+select veh.registration
+from vehicle veh, mot_test_current mtc, mot_test_current_rfr_map rfr
+where veh.id < 1000000
+and veh.registration is not null
+and mtc.vehicle_id = veh.id
+and mtc.prs_mot_test_id is null  -- only shows tests that do not have a Pass after Rectification at Station
+and mtc.id = rfr.mot_test_id
+and rfr.rfr_type_id=1 -- Advisory reason for Reason for Rejection
+and rfr.failure_dangerous=1 -- shows results for tests that fail for a dangerous reason
+and mtc.vehicle_id = veh.id
+and mtc.status_id=6 -- MOT test passed
+limit 10
