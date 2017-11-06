96dac7932923a7ea654cce244d912bf706d52cee
Dariusz Jerzewski <d.jerzewski@kainos.com>
2017-11-06 13:22:13 +0000

    Small fix to styling

--- src/main/java/uk/gov/dvsa/mot/framework/csv/CsvDocument.java
+++ src/main/java/uk/gov/dvsa/mot/framework/csv/CsvDocument.java
@@ -41,31 +41,31 @@ public class CsvDocument {
      * @return output as 2D string array.
      */
     public static ArrayList<ArrayList<String>> parse(String document) throws FailedToLoadCsvException {
-            boolean usesDoubleQuotes = false;
+        boolean usesDoubleQuotes = false;
 
-            //This pattern is to check if values are wrapped in double quotes
-            Pattern pattern = Pattern.compile("(\"([\\s]+)?,([\\s]+)?\")");
-            Matcher matcher = pattern.matcher(document);
-            int matchingCount = matcher.groupCount();
+        //This pattern is to check if values are wrapped in double quotes
+        Pattern pattern = Pattern.compile("(\"([\\s]+)?,([\\s]+)?\")");
+        Matcher matcher = pattern.matcher(document);
+        int matchingCount = matcher.groupCount();
 
-            String[] rows = document.split(System.getProperty("line.separator"));
-            ArrayList<ArrayList<String>> mappedCsv = new ArrayList<>();
+        String[] rows = document.split(System.getProperty("line.separator"));
+        ArrayList<ArrayList<String>> mappedCsv = new ArrayList<>();
 
-            if (matchingCount > 0) {
-                usesDoubleQuotes = true;
+        if (matchingCount > 0) {
+            usesDoubleQuotes = true;
+        }
+
+        for (int i = 0; i < rows.length; ++i) {
+            String[] values = rows[i].split( ",");
+
+            mappedCsv.add(new ArrayList<>());
+
+            for (int j = 0; j < values.length; ++j) {
+                String value = trimSpaces(values[j], usesDoubleQuotes);
+                mappedCsv.get(i).add(usesDoubleQuotes ? stripDoubleQuotesFromValue(value) : value);
             }
-
-            for (int i = 0; i < rows.length; ++i) {
-                String[] values = rows[i].split( ",");
-
-                mappedCsv.add(new ArrayList<>());
-
-                for (int j = 0; j < values.length; ++j) {
-                    String value = trimSpaces(values[j], usesDoubleQuotes);
-                    mappedCsv.get(i).add(usesDoubleQuotes ? stripDoubleQuotesFromValue(value) : value);
-                }
-            }
-            return mappedCsv;
+        }
+        return mappedCsv;
     }
 
     /**
