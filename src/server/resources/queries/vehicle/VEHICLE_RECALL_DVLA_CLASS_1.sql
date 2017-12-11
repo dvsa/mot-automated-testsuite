6b5d7aac347060b15747266d590da7584cd0dfaf
beckettk <karl.beckett@bjss.com>
2017-12-11 07:20:32 +0000

    [KB] sql fix for PP

--- src/server/resources/queries/vehicle/VEHICLE_RECALL_DVLA_CLASS_1.sql
+++ src/server/resources/queries/vehicle/VEHICLE_RECALL_DVLA_CLASS_1.sql
@@ -6,8 +6,11 @@ left join dvla_model_model_detail_code_map dmap on dmap.dvla_make_code = dmo.mak
 left join make ma on dmap.make_id = ma.id
 left join model mo on dmap.model_id = mo.id
 where v.id < 1000000
+and v.vehicle_id is null -- vehicle not used in MOT application yet
+and v.registration is not null -- nullable in PP/Prod
+and v.vin is not null -- nullable in PP/Prod
 and length(v.vin) > 5
 and ma.name is not null
 and mo.name is not null
-and v.eu_classification = 'L1'  -- DVLA moped
+and (v.eu_classification = 'L1' or v.eu_classification = 'L3')-- DVLA moped
 limit 10;
