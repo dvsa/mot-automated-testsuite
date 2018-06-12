9587270d9bcd88b42953ff27e218b5cbf17ca14e
Karl Beckett <karl.beckett@bjss.com>
2018-06-12 14:29:55 +0100

    More brakes tests added

--- src/main/java/uk/gov/dvsa/mot/fixtures/mot/TesterDoesStepDefinitions.java
+++ src/main/java/uk/gov/dvsa/mot/fixtures/mot/TesterDoesStepDefinitions.java
@@ -215,6 +215,15 @@ public class TesterDoesStepDefinitions implements En {
                         serviceBrakeOffsideAxle1, serviceBrakeNearsideAxle2,serviceBrakeOffsideAxle2,
                         parkingBrakeNearside, parkingBrakeOffside)));
 
+        And("^I enter single line class 4 roller results for vehicle weight of (\\d+) as service brake "
+                + "(\\d+),(\\d+),(\\d+),(\\d+) and parking brake (\\d+),(\\d+)$", (Integer weight,
+                   Integer serviceBrakeNearsideAxle1, Integer serviceBrakeOffsideAxle1,
+                   Integer serviceBrakeNearsideAxle2, Integer serviceBrakeOffsideAxle2,
+                   Integer parkingBrakeNearside, Integer parkingBrakeOffside) ->
+                handleBrakeResults(BrakeTestJourney.addSingleClass4RollerJourney(weight, serviceBrakeNearsideAxle1,
+                        serviceBrakeOffsideAxle1, serviceBrakeNearsideAxle2,serviceBrakeOffsideAxle2,
+                        parkingBrakeNearside, parkingBrakeOffside)));
+
         And("^I edit class 4 roller results for vehicle weight of (\\d+) as service brake "
                 + "(\\d+),(\\d+),(\\d+),(\\d+) and parking brake (\\d+),(\\d+)$", (Integer weight,
                     Integer serviceBrakeNearsideAxle1, Integer serviceBrakeOffsideAxle1,
@@ -854,6 +863,25 @@ public class TesterDoesStepDefinitions implements En {
         }
 
         /**
+         * Add single line class 4 brake test result - both service and parking brake using roller.
+         * @param weight                        The vehicle weight
+         * @param serviceBrakeNearsideAxle1     Weight applied by service brake to nearside axle 1
+         * @param serviceBrakeOffsideAxle1      Weight applied by service brake to offside axle 1
+         * @param serviceBrakeNearsideAxle2     Weight applied by service brake to nearside axle 2
+         * @param serviceBrakeOffsideAxle2      Weight applied by service brake to offside axle 2
+         * @param parkingBrakeNearside          Weight applied by parking brake to nearside
+         * @param parkingBrakeOffside           Weight applied by parking brake to offside
+         * @return The journey
+         */
+        static BrakeTestJourney addSingleClass4RollerJourney(int weight, int serviceBrakeNearsideAxle1,
+                   int serviceBrakeOffsideAxle1, int serviceBrakeNearsideAxle2, int serviceBrakeOffsideAxle2,
+                   int parkingBrakeNearside, int parkingBrakeOffside) {
+            return addPlateOrRollerJourney(BrakeTestJourneyType.AddSingleClass4ServiceAndParkingRollerResult, weight,
+                    serviceBrakeNearsideAxle1, serviceBrakeOffsideAxle1, serviceBrakeNearsideAxle2,
+                    serviceBrakeOffsideAxle2, parkingBrakeNearside, parkingBrakeOffside);
+        }
+
+        /**
          * Edit class 4 brake test result - both service and parking brake using roller.
          * @param weight                        The vehicle weight
          * @param serviceBrakeNearsideAxle1     Weight applied by service brake to nearside axle 1
@@ -1155,6 +1183,15 @@ public class TesterDoesStepDefinitions implements En {
 
                 breakLineType(journey.journeyType
                         == BrakeTestJourney.BrakeTestJourneyType.AddSingleClass4ServiceAndParkingRollerResult);
+
+                if (journey.journeyType
+                        == BrakeTestJourney.BrakeTestJourneyType.AddSingleClass4ServiceAndParkingRollerResult) {
+                    // And I click the "Single" radio button in fieldset "Brake line type"
+                    driverWrapper.selectRadioInFieldset("Brake line type", "Single");
+                } else {
+                    // And I click the "Dual" radio button in fieldset "Brake line type"
+                    driverWrapper.selectRadioInFieldset("Brake line type", "Dual");
+                }
                 // And I select "2 axles" in the "Number of axles" field
                 driverWrapper.selectOptionInField("2 axles", "Number of axles");
                 // And I press the "Next" button
