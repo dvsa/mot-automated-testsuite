384fff3a803fc849a1aa40efea481ad128c421b5
Karl Beckett <karl.beckett@bjss.com>
2018-06-18 11:34:26 +0100

    Updates to MOTH SQL to pull back correct data on all environments

--- src/server/resources/queries/vehicle/VEHICLE_REG_DANGEROUS.sql
+++ src/server/resources/queries/vehicle/VEHICLE_REG_DANGEROUS.sql
@@ -9,5 +9,6 @@ and rfr.failure_dangerous = 1 -- shows results for tests that fail for a dangero
 and rfr.rfr_type_id = 2 -- failure defect
 and mtc.status_id = 5 -- Failed test
 and mtc.mot_test_type_id = 1 -- Normal test
+and mtc.completed_date <= str_to_date('19/05/2018', '%d/%m/%Y') -- competed mot tests before EURW changes
 and not exists (select 1 from mot_test_current_rfr_map ads where mtc.id = ads.mot_test_id and ads.rfr_type_id in (1, 6)) -- no advisories
 limit 10
