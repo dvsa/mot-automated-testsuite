8bf68c8fc4e93f7749770c94c6d62b8ef48ad927
andrew-lai-bjss <andrew.lai@bjss.com>
2018-07-30 16:54:17 +0100

    Code refactor and tidy.

--- src/main/java/uk/gov/dvsa/mot/fixtures/generic/JsonApiStepDefinitions.java
+++ src/main/java/uk/gov/dvsa/mot/fixtures/generic/JsonApiStepDefinitions.java
@@ -18,6 +18,23 @@ public class JsonApiStepDefinitions implements En {
     /** The logger to use. */
     private static final Logger logger = LoggerFactory.getLogger(JsonApiStepDefinitions.class);
 
+    /**
+     * Enum for the Trade API search types.
+     */
+    private enum TradeApiSearchType {
+        TEST_NUMBER("number"), REGISTRATION_NUMBER("registration"), PAGE_NUMBER("page");
+
+        private String parameter;
+
+        public String getParameter() {
+            return this.parameter;
+        }
+
+        TradeApiSearchType(String parameter) {
+            this.parameter = parameter;
+        }
+    }
+
     private Response response;
 
     /**
@@ -27,63 +44,20 @@ public class JsonApiStepDefinitions implements En {
     @Inject
     public JsonApiStepDefinitions(WebDriverWrapper driverWrapper, Environment env) {
         Given("^I invoke the MOTH B2B API with registration \\{([^\\}]+)\\} and make \\{([^\\}]+)\\}$",
-                (String registration, String make) -> {
-
-                String url = env.getProperty("mothB2BUrl");
-
-                HashMap<String, String> headers = new HashMap<String, String>();
-                headers.put("X-Forwarded-For", "62.128.221.147");
-                headers.put("Accept", "application/json");
-                response = given().headers(headers).when().get(url + driverWrapper.getData(registration) + "/"
-                        + driverWrapper.getData(make)).andReturn();
-
-                logger.debug("RESPONSE:" + response.body().prettyPrint());
-            });
+                (String registration, String make) -> invokeMothB2B(driverWrapper.getData(registration),
+                        driverWrapper.getData(make), env));
 
         Given("^I invoke the Trade API search by test number with \\{([^\\}]+)\\}$",
-                (String testNumber) -> {
-
-                    String url = env.getProperty("tradeApiUrl");
-                    String apiKey = env.getProperty("tradeApiKey");
-
-                    HashMap<String, String> headers = new HashMap<String, String>();
-                    headers.put("x-api-key", apiKey);
-                    headers.put("Accept", "application/json");
-                    response = given().headers(headers).when().get(url + "?number="
-                            + driverWrapper.getData(testNumber)).andReturn();
-
-                    logger.debug("RESPONSE:" + response.body().prettyPrint());
-                });
+                (String testNumber) -> invokeTradeApi(TradeApiSearchType.TEST_NUMBER,
+                        driverWrapper.getData(testNumber), env));
 
         Given("^I invoke the Trade API search by registration number with \\{([^\\}]+)\\}$",
-                (String registrationNumber) -> {
-
-                    String url = env.getProperty("tradeApiUrl");
-                    String apiKey = env.getProperty("tradeApiKey");
-
-                    HashMap<String, String> headers = new HashMap<String, String>();
-                    headers.put("x-api-key", apiKey);
-                    headers.put("Accept", "application/json");
-                    response = given().headers(headers).when().get(url + "?registration="
-                            + driverWrapper.getData(registrationNumber)).andReturn();
-
-                    logger.debug("RESPONSE:" + response.body().prettyPrint());
-                });
+                (String registrationNumber) -> invokeTradeApi(TradeApiSearchType.REGISTRATION_NUMBER,
+                        driverWrapper.getData(registrationNumber), env));
 
         Given("^I invoke the Trade API search by page number with \"([^\"]+)\"$",
-                (String pageNumber) -> {
-
-                    String url = env.getProperty("tradeApiUrl");
-                    String apiKey = env.getProperty("tradeApiKey");
-
-                    HashMap<String, String> headers = new HashMap<String, String>();
-                    headers.put("x-api-key", apiKey);
-                    headers.put("Accept", "application/json");
-                    response = given().headers(headers).when().get(url + "?page="
-                            + pageNumber).andReturn();
-
-                    logger.debug("RESPONSE:" + response.body().prettyPrint());
-                });
+                (String pageNumber) -> invokeTradeApi(TradeApiSearchType.PAGE_NUMBER,
+                        driverWrapper.getData(pageNumber), env));
 
         Then("^The first response contains \"([^\"]+)\" equals \\{([^\\}]+)\\}$", (String name, String value) -> {
             response.then().body(name + "[0]", is(driverWrapper.getData(value)));
@@ -92,5 +66,40 @@ public class JsonApiStepDefinitions implements En {
         Then("^The response contains \"([^\"]+)\" equals \\{([^\\}]+)\\}$", (String name, String value) -> {
             response.then().body(name, is(driverWrapper.getData(value)));
         });
+    }
+
+    /**
+     * Invokes the MOTH B2B API endpoint with the given registration and make.
+     * @param registration  The vehicle registration
+     * @param make          The vehicle make
+     * @param env           The environmental variables
+     */
+    private void invokeMothB2B(String registration, String make, Environment env) {
+        String url = env.getProperty("mothB2BUrl");
+
+        HashMap<String, String> headers = new HashMap<String, String>();
+        headers.put("X-Forwarded-For", "62.128.221.147");
+        headers.put("Accept", "application/json");
+        response = given().headers(headers).when().get(url + registration + "/" + make).andReturn();
+
+        logger.debug("RESPONSE:" + response.body().prettyPrint());
+    }
+
+    /**
+     * Invokes the Trade API endpoint.
+     * @param searchType    The type of Trade API search to initiate
+     * @param value         The search value
+     * @param env           The environmental variables
+     */
+    private void invokeTradeApi(TradeApiSearchType searchType, String value, Environment env) {
+        String url = env.getProperty("tradeApiUrl");
+        String apiKey = env.getProperty("tradeApiKey");
+
+        HashMap<String, String> headers = new HashMap<String, String>();
+        headers.put("x-api-key", apiKey);
+        headers.put("Accept", "application/json");
+        response = given().headers(headers).when().get(url + "?" + searchType.getParameter() + "=" + value).andReturn();
+
+        logger.debug("RESPONSE:" + response.body().prettyPrint());
     }
 }
