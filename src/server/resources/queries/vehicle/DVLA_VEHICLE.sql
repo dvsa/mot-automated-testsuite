76a353cbff0b97d1aed6de77450ae2d56a2d5125
andrew-lai-bjss <andrew.lai@bjss.com>
2018-08-20 12:31:20 +0100

    Work around for know defect BL-8277

--- src/server/resources/queries/vehicle/DVLA_VEHICLE.sql
+++ src/server/resources/queries/vehicle/DVLA_VEHICLE.sql
@@ -8,4 +8,9 @@ and not exists (	-- No vehicle exists in the vehicle table with same reg and vin
 	where v.registration = d.registration
 	and v.vin = d.vin
 	)
+and not exists (
+    select 1 from mot_test_current mtc
+    where d.id = mtc.vehicle_id
+    and mtc.status_id = 5 -- failed test
+    ) -- BL-8277 Due to know issue, exclude vehicles that have the same id as a failed dvsa vehicle
 limit 50
