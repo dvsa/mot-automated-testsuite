40eb09b2d95b0ae113f0f132924f69a9cfc5412c
John Reynolds <john.reynolds@bjss.com>
2018-10-23 12:34:18 +0100

    Update the sql to add mileage details

--- src/server/resources/queries/vehicle/VEHICLE_CLASS_4_BEFORE_1960_WITH_EXPIRED_MOT.sql
+++ src/server/resources/queries/vehicle/VEHICLE_CLASS_4_BEFORE_1960_WITH_EXPIRED_MOT.sql
@@ -1,12 +1,19 @@
-select d.registration as registration, d.vin as vin, 0 as mileage
-from dvla_vehicle d
-where d.vehicle_id in
+select d.registration as registration, d.vin as vin, mtc.odometer_value as mileage
+from dvla_vehicle d, (
+     select max(id) as id, vehicle_id  from mot_test_current
+     group by vehicle_id
+     limit 100000) as latest_mot,
+     mot_test_current mtc
+where mtc.id = latest_mot.id
+and d.vehicle_id = latest_mot.vehicle_id
+and d.vehicle_id in
     (select v.id
 	from vehicle v
 	where v.first_registration_date < "1960-01-01")
 	and (select max(m.expiry_date)
 		from mot_test_current m
 		where m.vehicle_id = d.vehicle_id
-		and m.mot_test_type_id = 1
-		and m.status_id = 6) < now()
+	    and m.mot_test_type_id = 1
+		and m.status_id = 6
+		) < now()
 		limit 10;
