2ab9cc8ec2f4dcec10cb80d1bceaab82c88ed93e
Karl Beckett <karl.beckett@bjss.com>
2018-12-11 14:08:07 +0000

    quick update to SQL to exclude vehicles that have already been modified by automation

--- src/server/resources/queries/vehicle/VEHICLE_CLASS_4.sql
+++ src/server/resources/queries/vehicle/VEHICLE_CLASS_4.sql
@@ -14,6 +14,8 @@ and odometer_result_type = 'OK'
 and veh.registration not like "%-%" -- exclude dodgy test data on ACPT
 and veh.registration is not null -- nullable in PP/Prod
 and veh.registration <> 'R3GHAU5' -- Exclude vehicles that have already been modified by automation
+and veh.registration <> 'R3GHA01' -- Exclude vehicles that have already been modified by automation
+and veh.registration <> 'R3GHDVL5' -- Exclude vehicles that have already been modified by automation
 and veh.vin is not null -- nullable in PP/Prod
 and mtc.vehicle_version = veh.version
 and not exists (
