af22923e84c1b68efb6197f133320e141a7d279f
John Reynolds <john.reynolds@bjss.com>
2019-01-09 17:42:11 +0000

    Added a test to cover Pre-1960 vehicle with current MOT

---
+++ src/server/resources/queries/vehicle/VEHICLE_CLASS_4_BEFORE_1960_WITH_AN_MOT.sql
@@ -0,0 +1,18 @@
+select d.registration as registration, d.vin as vin, mtc.odometer_value as mileage
+from dvla_vehicle d, (
+     select max(id) as id, vehicle_id  from mot_test_current
+     group by vehicle_id
+     limit 100000) as latest_mot,
+     mot_test_current mtc
+where d.registration is not null -- nullable in PP/Prod
+and d.vin is not null -- nullable in PP/Prod
+and mtc.id = latest_mot.id
+and d.vehicle_id = latest_mot.vehicle_id
+and d.first_registration_date < '1960-01-01' -- select a vehicle which requires an optional MOT
+and d.vehicle_id in (select vehicle_id from mot_test_current where expiry_date > now())
+and not exists (
+    select 1 from mot_test_current mtc
+    where d.id = mtc.vehicle_id
+    and mtc.status_id not in (4,5) -- exclude vehicles whose latest status is under test or failed
+) -- BL-8277 Due to know issue, exclude vehicles that have the same id as a failed dvsa vehicle
+limit 5
