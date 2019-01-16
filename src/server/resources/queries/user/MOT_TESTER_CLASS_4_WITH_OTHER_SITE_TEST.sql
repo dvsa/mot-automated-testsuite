dee5a50311acde75988f8cf6aad8348d6c6cea20
Karl Beckett <karl.beckett@bjss.com>
2019-01-16 16:57:59 +0000

    updated the correct SQL files

--- src/server/resources/queries/user/MOT_TESTER_CLASS_4_WITH_OTHER_SITE_TEST.sql
+++ src/server/resources/queries/user/MOT_TESTER_CLASS_4_WITH_OTHER_SITE_TEST.sql
@@ -1,76 +1,56 @@
-/* Description */
-/* This SQL is to obtain an active MOT tester, with  valid  2FA, who can perform an MOT test on class 4 only */
-
-
-SELECT DISTINCT
-    p.username,
-    s.site_number AS site
-FROM
-    person p
-    -- Check that user has logged in with 2fa
-    JOIN person_security_card_map pscm
-        ON p.id = pscm.person_id
-    JOIN security_card sc
-        ON pscm.security_card_id = sc.id
-    JOIN security_card_drift scd
-        ON sc.id = scd.security_card_id
-    -- Checking tester is authorised to test
-    JOIN auth_for_testing_mot aftm
-        ON p.id = aftm.person_id
-    -- Checking site is authorised to test
-    JOIN site_business_role_map sbrm
-        ON p.id = sbrm.person_id
-    JOIN site s
-        ON sbrm.site_id = s.id
-    JOIN auth_for_testing_mot_at_site afts
-        ON s.id = afts.site_id
-    -- Checking AE is authorised to test
-    JOIN organisation_site_map osm
-        ON s.id = osm.site_id
-    JOIN organisation o
-        ON osm.organisation_id = o.id
-    JOIN auth_for_ae afa
-        ON o.id = afa.organisation_id
-WHERE
-    -- Check the secuirty card assigned to them is active
-    sc.security_card_status_lookup_id = 1
-    -- Check last 2FA input was within +/-2 windows
-    AND scd.last_observed_drift BETWEEN -60 AND 60
-    -- Check user doesn't need to claim account
-    AND p.is_account_claim_required = 0
-    -- Check user doesn't need to change password
-    AND p.is_password_change_required = 0
-    -- User is a tester
-    AND sbrm.site_business_role_id = 1
-    -- Tester is active
-    AND sbrm.status_id = 1
-    -- Tester authorised to test
-    AND aftm.status_id = 9
-    -- Tester authorised to test class 4 vehicles
-    AND aftm.vehicle_class_id = 4
-    -- Site authorised to test class 4 vehicles
-    AND afts.vehicle_class_id = 4
-    -- AE is authorised to test
-    AND afa.status_id = 2
-    -- AE linked to site
-    AND osm.status_id = 2
-    -- AE has slots available
-    AND o.slots_balance > 15
-    -- Check users have acknowledge all special notices
-    -- Check users don't have active tests
-    AND p.id NOT IN (
-        SELECT
-            person_id
-        FROM
-            special_notice
-        WHERE
-            is_acknowledged = 0
-    
-    UNION
-        SELECT
-            person_id
-        FROM
-            mot_test_current
-        WHERE 
-            status_id = 4
-    );
+select distinct(p.username) as username, s.name as site, v.registration as registration
+from person p, auth_for_testing_mot aftm, organisation o,
+  organisation_site_map osm, site s, auth_for_testing_mot_at_site afts,
+  site_business_role_map sbrm, auth_for_ae afa, security_card sc,
+  person_security_card_map pscm, mot_test_current mtc, vehicle v
+where aftm.person_id = p.id
+and aftm.vehicle_class_id = 4 -- only cars
+and aftm.status_id = 9 -- only qualified testing authorisations
+and p.is_account_claim_required = 0
+and p.is_password_change_required = 0
+and o.id = osm.organisation_id
+and s.id = osm.site_id
+and s.id = afts.site_id
+and afts.vehicle_class_id = 4 -- only cars
+and afts.status_id = 2 -- only valid site authorisations
+and sbrm.site_id = s.id
+and sbrm.person_id = p.id
+and sbrm.site_business_role_id = 1 -- only testers
+and sbrm.status_id = 1 -- Only Active tester roles
+and afa.organisation_id = o.id
+and afa.status_id = 2 -- only valid ae authorisations
+and o.slots_balance > 15 -- ae's with slots available
+and p.id = pscm.person_id
+and sc.id = pscm.security_card_id
+and sc.security_card_status_lookup_id = 1 -- only assigned cards
+and mtc.site_id <> s.id
+and mtc.status_id = 6 -- Passed tests only
+and mtc.mot_test_type_id = 1  -- Normal tests only
+and mtc.vehicle_id = v.id
+and mtc.completed_date > DATE_SUB(CURDATE(), INTERVAL 10 DAY)  -- recent tests only, fixes data issues with new certs
+and mtc.document_id IS NOT NULL  -- exclude where there are no MOT certificates
+and mtc.id = (select max(lmtc.id) from mot_test_current lmtc where lmtc.vehicle_id = v.id) -- Checking only the latest test for vehicle
+and not exists (
+    select 1 from vehicle v2
+    where v2.registration = v.registration
+    group by v2.registration
+    having count(v2.registration) > 1 -- exclude where same registration has been entered as different vehicles
+)
+and not exists (
+    select 1 from vehicle v2
+    where v2.vin = v.vin
+    group by v2.vin
+    having count(v2.vin) > 1 -- exclude where same vin has been entered as different vehicles
+)
+and not exists ( -- not all security_card have a corresponding security_card_drift
+  select 1 from security_card_drift scd
+  where sc.id = scd.security_card_id
+  and (scd.last_observed_drift > 60 or scd.last_observed_drift < -60) -- no drift beyond +/-2
+)
+and not exists (
+  select 1 from mot_test_current mtc
+  where p.id = mtc.last_updated_by
+  and mtc.status_id = 4 -- exclude any testers with active tests
+)
+and p.username is not null -- exclude dodgy test data
+limit 100
