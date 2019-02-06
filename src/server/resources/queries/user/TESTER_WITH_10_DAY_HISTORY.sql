596f1e63a7f86e11f5bbb0808a1f8fd842194b76
John Reynolds <john.reynolds@bjss.com>
2019-02-06 13:30:17 +0000

    fix to TESTER WITH 10 DAY HISTORY

--- src/server/resources/queries/user/TESTER_WITH_10_DAY_HISTORY.sql
+++ src/server/resources/queries/user/TESTER_WITH_10_DAY_HISTORY.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT p.username
 FROM person p, mot_test_current mtc
-WHERE DATE(mtc.completed_date) > date_sub(CURDATE(), INTERVAL 10 DAY)
+WHERE mtc.completed_date > date_sub(CURDATE(), INTERVAL 10 DAY)
 AND mtc.mot_test_type_id IN (1,9)
 AND p.id = mtc.person_id
 AND p.username IS NOT NULL
