56d55cbb876858f29b2b957e319c45dce25ecced
John Reynolds <john.reynolds@bjss.com>
2019-02-07 14:51:12 +0000

    added test type id in rather than not equal

--- src/server/resources/queries/site/SITE_RECENT.sql
+++ src/server/resources/queries/site/SITE_RECENT.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT s.site_number, s.name as site_name
 FROM site s, site_contact_detail_map d_map, mot_test_current mtc
 WHERE mtc.mot_test_type_id IN (1,9)
 AND mtc.completed_date > date_sub(CURDATE(), INTERVAL 3 WEEK)
-AND mtc.mot_test_type_id != "4"
+AND mtc.mot_test_type_id in ('1','9')
 AND s.id = mtc.site_id
 AND d_map.site_id = s.id
 AND d_map.contact_detail_id IS NOT NULL
