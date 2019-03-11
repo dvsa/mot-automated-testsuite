a190592ea1da013b63cee64bc6a5e75fb92f998d
John Reynolds <john.reynolds@bjss.com>
2019-03-11 14:43:51 +0000

    chnage optional MOT sql to call vehicle table rather than DVLA_vehicle

--- src/server/resources/queries/vehicle/VEHICLE_CLASS_4_BEFORE_1960_WITH_AN_MOT.sql
+++ src/server/resources/queries/vehicle/VEHICLE_CLASS_4_BEFORE_1960_WITH_AN_MOT.sql
@@ -1,18 +1,40 @@
-select d.registration as registration, d.vin as vin, mtc.odometer_value as mileage
-from dvla_vehicle d, (
-     select max(id) as id, vehicle_id  from mot_test_current
-     group by vehicle_id
-     limit 1000000) as latest_mot,
-     mot_test_current mtc
-where d.registration is not null -- nullable in PP/Prod
-and d.vin is not null -- nullable in PP/Prod
-and mtc.id = latest_mot.id
-and d.vehicle_id = latest_mot.vehicle_id
-and d.first_registration_date < '1960-01-01' -- select a vehicle which requires an optional MOT
-and d.vehicle_id in (select vehicle_id from mot_test_current where expiry_date > now())
-and not exists (
-    select 1 from mot_test_current mtc
-    where d.id = mtc.vehicle_id
-    and mtc.status_id not in (4,5) -- exclude vehicles whose latest status is under test or failed
-) -- BL-8277 Due to know issue, exclude vehicles that have the same id as a failed dvsa vehicle
-limit 5
+SELECT
+       veh.registration,
+       veh.vin,
+       mtc.odometer_value
+  FROM vehicle veh
+  JOIN mot_test_current mtc ON mtc.vehicle_id = veh.id
+  JOIN model_detail md ON md.id = veh.model_detail_id
+  JOIN (SELECT MAX(submitted_date) AS max_date,
+               vehicle_id
+          FROM mot_test_current
+         GROUP BY vehicle_id
+       LIMIT 5000) as lmot -- Latest MOT
+    ON lmot.max_date = mtc.submitted_date AND lmot.vehicle_id = mtc.vehicle_id
+ WHERE md.vehicle_class_id = 4 -- cars only
+   AND datediff(curdate(), mtc.completed_date) > 15 -- excludes retests, tests that are over 8 days old
+   AND veh.first_used_date < str_to_date('01/01/1960', '%d/%m/%Y') -- select vehicles which require an optional MOT
+   AND mtc.completed_date IS NOT NULL -- excludes first-time tests
+   AND mtc.status_id not in (4,5) -- exclude vehicles whose latest status is under test or failed
+   AND mtc.mot_test_type_id = 1 -- Normal MOT test
+   AND mtc.odometer_result_type = 'OK'
+   AND mtc.odometer_value < 994999
+   AND mtc.status_id = 6 -- Passed MOT tests
+   AND veh.vin IS NOT NULL
+   AND veh.registration NOT IN ('R3GHAU5', 'R3GHA01', 'R3GHDVL5', 'DVLA903') -- Exclude vehicles that have already been modified by automation
+   AND veh.registration NOT LIKE '%-%' -- exclude dodgy test data on ACPT
+   AND veh.registration IS NOT NULL -- nullable in PP/Prod
+   AND NOT EXISTS (
+      SELECT 1 FROM vehicle v
+      WHERE v.registration = veh.registration
+      GROUP BY v.registration
+      HAVING COUNT(v.registration) > 1 -- exclude where same registration has been entered as different vehicles
+      )
+    AND NOT EXISTS (
+      SELECT 1 FROM vehicle v
+      WHERE v.vin = veh.vin
+      GROUP BY v.vin
+      HAVING COUNT(v.vin) > 1 -- exclude where same vin has been entered as different vehicles
+      )
+ORDER BY RAND()
+LIMIT 5
