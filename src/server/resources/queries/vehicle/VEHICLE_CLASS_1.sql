7bba801885b7f88163f96f64e227941f9190c627
Karl Beckett <karl.beckett@bjss.com>
2019-08-01 14:33:45 +0100

    SQL change to remove RAND from some SQL files

--- src/server/resources/queries/vehicle/VEHICLE_CLASS_1.sql
+++ src/server/resources/queries/vehicle/VEHICLE_CLASS_1.sql
@@ -1,34 +1,27 @@
-SELECT
-       veh.registration,
-       veh.vin,
-       mtc.odometer_value
-  FROM vehicle veh
-  JOIN mot_test_current mtc ON mtc.vehicle_id = veh.id
-  JOIN model_detail md ON md.id = veh.model_detail_id
-  JOIN (SELECT MAX(submitted_date) AS max_date,
-               vehicle_id
-          FROM mot_test_current
-         GROUP BY vehicle_id
-       LIMIT 10000) as lmot -- Latest MOT
-    ON lmot.max_date = mtc.submitted_date AND lmot.vehicle_id = mtc.vehicle_id
- WHERE md.vehicle_class_id = 1 -- class 1 only
-   AND mtc.status_id not in (4,5) -- exclude vehicles whose latest status is under test or failed
-   AND mtc.odometer_result_type = 'OK'
-   AND mtc.odometer_value < 994999
-   AND veh.vin IS NOT NULL
-   AND veh.registration NOT IN ('R3GHAU5', 'R3GHA01', 'R3GHDVL5', 'DVLA903') -- Exclude vehicles that have been modified by automation
-   AND veh.registration NOT LIKE '%-%' -- exclude dodgy test data on ACPT
+SELECT veh.registration, veh.vin, mtc.odometer_value
+ FROM vehicle AS veh
+ JOIN (SELECT ROUND(RAND() * (SELECT MAX(id) FROM vehicle)) AS id ) AS x       ON veh.id >= x.id
+ JOIN mot_test_current AS mtc                                                  ON veh.id = mtc.vehicle_id
+ JOIN model_detail AS md                                                       ON veh.model_detail_id = md.id
+WHERE veh.vin IS NOT NULL
+  AND veh.registration NOT IN ('R3GHAU5', 'R3GHA01', 'R3GHDVL5', 'DVLA903')                                                         -- Exclude vehicles that have already been modified by automation
+  AND veh.registration NOT LIKE '%-%'                                                                                               -- exclude dodgy test data on ACPT
+  AND NOT EXISTS (
+     SELECT 1 FROM vehicle v
+     WHERE v.registration = veh.registration
+     GROUP BY v.registration
+     HAVING COUNT(v.registration) > 1                                                                                               -- exclude where same registration has been entered as different vehicles
+     )
    AND NOT EXISTS (
-      SELECT 1 FROM vehicle v
-      WHERE v.registration = veh.registration
-      GROUP BY v.registration
-      HAVING COUNT(v.registration) > 1 -- exclude where same registration has been entered as different vehicles
-      )
-    AND NOT EXISTS (
-      SELECT 1 FROM vehicle v
-      WHERE v.vin = veh.vin
-      GROUP BY v.vin
-      HAVING COUNT(v.vin) > 1 -- exclude where same vin has been entered as different vehicles
-      )
-ORDER BY RAND()
-LIMIT 50
+     SELECT 1 FROM vehicle v
+     WHERE v.vin = veh.vin
+     GROUP BY v.vin
+     HAVING COUNT(v.vin) > 1                                                                                                        -- exclude where same vin has been entered as different vehicles
+     )
+  AND mtc.submitted_date = (SELECT MAX(mtc1.submitted_date) FROM mot_test_current mtc1 WHERE mtc1.vehicle_id = mtc.vehicle_id)
+  AND mtc.mot_test_type_id = 1                                                                                                      -- Normal MOT test
+  AND mtc.odometer_result_type = 'OK'
+  AND mtc.odometer_value < 994999
+  AND mtc.status_id = 6                                                                                                             -- Passed MOT tests
+  AND md.vehicle_class_id = 1                                                                                                       -- cars only
+LIMIT 100;
